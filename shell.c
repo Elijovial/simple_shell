d835e7f3a10edb9e8d5f75658174115aeb0cbede
Elijovial <omeehelijah345@gmail.com>
2023-07-23 13:27:01 -0700

    task

--- shell.c
+++ shell.c
@@ -15,8 +15,8 @@
 int main(void)
 {
 	char *input = NULL, *args[MAX_LENGTH];
-	size_t n = 0, i = 0;
-	char *token = strtok(input, "\n\t\r ");
+	size_t n = 0;
+	int result, i = 0;
 	pid_t pid;
 
 	while (1)
@@ -39,6 +39,7 @@ int main(void)
 		}
 		else if (pid == 0)
 		{
+			char *token = strtok(input, "\n\t\r ");
 			while (token != NULL)
 			{
 				args[i] = token;
@@ -47,7 +48,17 @@ int main(void)
 			}
 			args[i] = NULL;
 			execvp(args[0], args);
-			fprintf(stderr, "Exec failed\n");
+			result = strcmp(input, "$?");
+			if (result == 0)
+			{
+				fprintf(stderr, "%s: %d: 0: not found\n", getenv("_"), i);
+			}
+			else if (result == -1 && result == 1)
+			{
+				fprintf(stderr, "%s: %d: %d: %s: not found\n", getenv("_"), i, WEXITSTATUS(system(args[0])), args[0]);
+                                }
+			else
+				fprintf(stderr,  "%s: %d: %s: not found\n", getenv("_"), i, args[0]);
 			return (1);
 		}
 		else
